76985212b713509ce9997aa1a105c9d40d4e910c
Isaac Phiri <isaacphiri315@gmail.com>
2022-12-23 22:49:07 +0000

    Deploy

---
+++ install_fab.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt-get install libffi-dev -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install build-essential -y
+sudo apt-get install python -y
+sudo apt-get install libpython3-dev -y
+pip3 install pyparsing
+pip3 install appdirs
+pip3 install setuptools
+pip3 install cryptography
+pip3 install bcrypt
+pip3 install PyNaCl
+pip3 install Fabric
